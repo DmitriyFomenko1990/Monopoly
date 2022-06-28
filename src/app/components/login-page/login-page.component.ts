bf3e451b694d9a85c9859280f61b984b15ebe74b
Dmitriy <dmitriy.f@devhouse.pro>
2022-06-28 22:19:09 +0300

    add login page

---
+++ src/app/components/login-page/login-page.component.ts
@@ -0,0 +1,84 @@
+import {Component, Input, OnInit, ViewChild} from '@angular/core';
+import {HttpService} from "../../services/http.service";
+import {UserType} from "../../types";
+import { ToastrService } from 'ngx-toastr'
+import autorization from "../../services/autorization.service";
+import {Router} from "@angular/router";
+//import TokenService from '../../services/token.service';
+
+@Component({
+  selector: 'app-login-page',
+  templateUrl: './login-page.component.html',
+  styleUrls: ['./login-page.component.scss'],
+  providers: [HttpService]
+})
+export class LoginPageComponent implements OnInit {
+  loginModel;
+  singinModel;
+  loginForm: boolean = true;
+  users: UserType[] | undefined;
+
+  constructor(private httpService: HttpService,
+              private toastr: ToastrService,
+              private router: Router
+  ) {
+    this.loginModel = {
+      password: '',
+      login: '',
+    }
+    this.singinModel = {
+      sex: "male",
+      password: '',
+      login: '',
+      name: '',
+    };
+  }
+
+  async getUser(){
+    try {
+      await this.httpService.getUsers()
+        .subscribe( data => this.users = data );
+    } catch (err: any) {
+      console.log(err)
+    }
+  }
+
+  login() {
+     this.httpService.getUsers()
+      .subscribe( data => {
+        this.users = data
+        let user = this.users?.find((user: UserType) => user.login === this.loginModel.login);
+        if (!user || user.password !== this.loginModel.password) {
+          this.toastr.error("Login or password is invalid");
+          return
+        }
+        const {password, ...userWithoutPass} = user
+        autorization.setCookie('monopolyToken', JSON.stringify(userWithoutPass));
+        this.router.navigate(['/game-page']);
+      } );
+  }
+
+  singIn():void {
+    this.httpService.getUsers()
+      .subscribe( data => {
+        this.users = data
+        let user = this.users?.find((user: UserType) => user.login === this.singinModel.login);
+        if (user) {
+          this.toastr.error("You are already registered");
+          return
+        }
+        user = {...this.singinModel, id: 10}
+        this.httpService.createNewUser( user )
+          .subscribe(res => {
+            console.log(res)
+          })
+        const {password, ...userWithoutPass} = user
+        autorization.setCookie('monopolyToken', JSON.stringify(userWithoutPass));
+        this.router.navigate(['/game-page']);
+        debugger
+      } );
+  }
+
+  ngOnInit(): void {
+  }
+}
