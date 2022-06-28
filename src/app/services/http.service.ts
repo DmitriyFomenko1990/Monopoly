ab10598592e9c2750056b389b5d48a652aa0f7d2
Dmitriy <dmitriy.f@devhouse.pro>
2022-06-28 22:17:26 +0300

    add game api and refactoring

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -1,23 +1,33 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {filter, Observable} from "rxjs";
-import {UserType} from "../types";
+import {UserType, GameType} from "../types";
 import {map} from "rxjs/operators";
 
 @Injectable()
 export class HttpService{
-
+  url: string = 'http://localhost:3000'
   constructor(private http: HttpClient){ }
 
   getUsers(): Observable<UserType[]>{
-    return this.http.get<UserType[]>('http://localhost:3000/users');
+    return this.http.get<UserType[]>(`${this.url}/users`);
   }
 
   createNewUser(user: UserType){
-    return this.http.post<any>('http://localhost:3000/users',  user)
+    return this.http.post<any>(`${this.url}/users`,  user)
       .pipe(map((res:any) => {
         return res
       }))
   }
 
+  getGames(): Observable<GameType[]>{
+    return this.http.get<GameType[]>(`${this.url}/games`);
+  }
+
+  createNewGame(game: GameType){
+    return this.http.post<any>(`${this.url}/games`,  game)
+      .pipe(map((res:any) => {
+        return res
+      }))
+  }
 }
